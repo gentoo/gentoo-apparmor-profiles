30a4503514f2b3b3dcb1b2b4671022c494601a73
Amir Guindehi <amir@datacore.ch>
2017-03-28 09:48:31 +0200

    updated n9 backuo bash.

--- n9.backup.bin.bash
+++ n9.backup.bin.bash
@@ -1,9 +1,11 @@
-# Last Modified: Mon Mar 27 11:22:14 2017
+# Last Modified: Tue Mar 28 09:44:07 2017
 #include <tunables/global>
 
 /n9.backup/bin/bash {
-
   #include <abstractions/base>
+
+  @{DATA_N9}/bin/* mrix,
+  @{DATA_N9}/usr/bin/* ixr,
 
   @{DATA_N9}/ r,
   @{DATA_N9}/data.dom0/ r,
@@ -12,10 +14,10 @@
   @{DATA_N9}/data.dom0/latest/ r,
   @{DATA_N9}/data.dom0/latest/** rw,
 
-  @{DATA_N9}/bin/* mrix,
+  @{DATA_N9}/tmp/.tarcmd w,
+
   @{DATA_N9}/dev/* rw,
   @{DATA_N9}/etc/* r,
-
   @{DATA_N9}/lib/* mr,
 
 }
